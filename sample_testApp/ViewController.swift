4b3ddeb27bded71a196458285086f3a619c4302d
Deekshith Vijaykumar <deekshith-dv@ibjapan.jp>
2022-11-09 11:38:55 +0900

    Initial Commit

---
+++ sample_testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sample_testApp
+//
+//  Created by ビジャイクマール デックシット ディッバダハッリ on 2022/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
